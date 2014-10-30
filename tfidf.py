6109c4c6c5bd2b19c6241836fcdb9e8f4a9dbc20
Théophane Hufschmitt <huf31@gmx.fr>
2014-10-30 15:00:08 +0100

    Refactoring
    * Inclusion of the number of documents in she sql database
    * Refactored the class

--- tfidf.py
+++ tfidf.py
@@ -2,57 +2,59 @@
 
 # Simple tf-idf library
 
-import sys
-import os.path
 import sqlite3
-import configparser
 import nltk
+
 
 class tfIdf:
     """
     The tf-idf class
     """
 
-    def __init__(self, database = None, configFile = None):
+    def __init__(self, database=None):
         if not database:
             self.database = "tfidf.sql"
         else:
             self.database = database
 
-        if not configFile:
-            self.configFile = "tfidf.cfg"
-        else:
-            self.configFile = configFile
-
-        self.db = sqlite3.connect(self.database)
-        self.config = configparser.ConfigParser()
         self.defaultFreq = 5
 
-        self.read_config()
+        self.init_database()
 
-    def read_config(self):
+    def init_database(self):
         """
-        Reads the config file
+        Fetches the state from the database, creating it if needed
         """
-        if os.path.isfile(self.configFile):
-            self.config.read(self.configFile)
+        db = sqlite3.connect(self.database)
+        c = db.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS state (state_variable char(64),"
+                  "state_value int)")
+        c.execute("CREATE TABLE IF NOT EXISTS words (word char(50), "
+                  "number_occurences int)")
+        c.execute("SELECT state_value FROM state WHERE state_variable ="
+                  "\"cardCorpus\"")
+        qRes = c.fetchone()
+        if (qRes):
+            self.cardCorpus, = qRes
         else:
-            print("Config file does not exists, creating it", file=sys.stderr)
-            self.__create_config()
+            self.cardCorpus = 0
+            c.execute("INSERT INTO state (state_variable, state_value)"
+                      "values (\"cardCorpus\", 0)")
 
-    def __create_config(self):
-        self.config['STATE'] = {'cardCorpus' : 0}
-
-        with open(self.configFile, 'w') as configStream:
-            self.config.write(configStream)
+        db.commit()
+        db.close()
 
     def increase_corpus_cardinal(self):
         """
         Tells that a file has been added to the corpus
         """
-        self.config['STATE']['cardCorpus'] = str(int(self.config['STATE']['cardCorpus']) + 1)
-        with open(self.configFile, 'w') as configStream:
-            self.config.write(configStream)
+        self.cardCorpus += 1
+        db = sqlite3.connect(self.database)
+        c = db.cursor()
+        c.execute("UPDATE state SET state_value = " + str(self.cardCorpus) +
+                  " WHERE state_variable = \"cardCorpus\"")
+        db.commit()
+        db.close()
 
     def add_to_corpus(self, stream):
         """
@@ -70,45 +72,61 @@ class tfIdf:
         self.parse_line(string)
 
     def parse_line(self, line):
-        known_words = {} # The words in the document that are part of the dictionnary
-        new_words = {} # The words in the document that have never been met before
-        c = self.db.cursor()
-        cardCorpus = int(self.config['STATE']['cardCorpus']) 
+        known_words = {}
+        # The words in the document that are part of the dictionnary
+        new_words = {}
+        # The words in the document that have never been met before
+        cardCorpus = int(self.cardCorpus)
+        # Number of documents in the corpus
+        db = sqlite3.connect(self.database)
+        c = db.cursor()
         tfidfs = {}
         tokens = [word.lower() for word in nltk.word_tokenize(line)]
         searchQuery = "SELECT number_occurences FROM words WHERE word =?"
+
         for token in tokens:
             if token not in known_words and token not in new_words:
                 c.execute(searchQuery, (token,))
                 fetch = c.fetchone()
                 if fetch:
                     nbOcc, = fetch
-                    known_words[token] = {'occurencesInText': 1, 'occurencesInCorpus': int(nbOcc)}
+                    known_words[token] = {'occurencesInText': 1,
+                                          'occurencesInCorpus': int(nbOcc)}
                 else:
-                    new_words[token] = {'occurencesInText': 1, 'occurencesInCorpus': 0}
+                    new_words[token] = {'occurencesInText': 1,
+                                        'occurencesInCorpus': 0}
             elif token in known_words:
                 known_words[token]['occurencesInText'] += 1
             else:
                 new_words[token]['occurencesInText'] += 1
 
         cardTxt = len(known_words) + len(new_words)
-        
         for token in known_words:
-            tfidfs[token] = (known_words[token]['occurencesInText']/cardTxt) * (cardCorpus/ known_words[token]['occurencesInCorpus'])
+            tfidfs[token] = ((known_words[token]['occurencesInText']/cardTxt) *
+                             (cardCorpus /
+                              known_words[token]['occurencesInCorpus']))
         for token in new_words:
-            tfidfs[token] = (new_words[token]['occurencesInText']/cardTxt) * self.defaultFreq
+            tfidfs[token] = ((new_words[token]['occurencesInText']/cardTxt) *
+                             self.defaultFreq)
+        db.close()
 
-        for word in sorted(tfidfs, key =tfidfs.get, reverse=False):
+        for word in sorted(tfidfs, key=tfidfs.get, reverse=False):
             print(word, tfidfs[word])
-        self.add_to_db(c, known_words, new_words)
 
-    def add_to_db(self, c, known_words, new_words):
-        updateQuery = "UPDATE words SET number_occurences= number_occurences + 1 WHERE word=:word"
-        insertQuery = "INSERT INTO words (word, number_occurences) values (:word, 1)"
+        self.add_to_db(known_words, new_words)
+
+    def add_to_db(self, known_words, new_words):
+        updateQuery = ("UPDATE words SET number_occurences="
+                       "number_occurences + 1 WHERE word=:word")
+        insertQuery = ("INSERT INTO words (word, number_occurences)"
+                       "values (:word, 1)")
+        db = sqlite3.connect(self.database)
+        c = db.cursor()
         for token, nbOcc in known_words.items():
             c.execute(updateQuery, {"word": token})
 
         for token, nbOcc in new_words.items():
             c.execute(insertQuery, {"word": token})
 
-        self.db.commit()
+        db.commit()
+        db.close()
